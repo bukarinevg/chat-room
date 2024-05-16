ed2b9b675ec82784bae8d146f1e1f48b80bef6a6
bukarinevgeni <veliknon@gmail.com>
2024-05-16 16:50:40 +0300

    add scss

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,11 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Chat Room',
+  };
+ 
 export default function Page(){
-    return(
+    return (
         <div className="p-3">
             <h1>Room</h1>
             <article className="ml-5">
                 <p>Room</p>
             </article>
         </div>
-
     );
 }
