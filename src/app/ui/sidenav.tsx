ec0ce135124594263beee4ad2a023e1d2660b379
bukarinevgeni <veliknon@gmail.com>
2024-05-22 20:08:44 +0300

    add page chat and login

--- src/app/ui/sidenav.tsx
+++ src/app/ui/sidenav.tsx
@@ -3,39 +3,38 @@
 import '@styles/sidenav.scss';
 import React from 'react';
 import { useState } from 'react';
-// import SidenavMenu from '@ui/sidenav-menu';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Sidenav(){
     const [open, setOpen] = useState(false);
 
-    function toggleSidenav(): void{
-        setOpen(!open);
-    }
-
     return(
         <>
 
-            <div className='sidenav'>
-                {/* <FontAwesomeIcon 
-                    className='sidenav__icon'
-                    icon={faBars}                      
-                    aria-expanded={open} 
-                    onClick={toggleSidenav}
-                    aria-controls="sidenav-menu" 
-                    /> */}
-             
-             
+            <div 
+                className='sidenav'  
+                onMouseEnter={() => setOpen(true) } 
+                onMouseLeave={() => setOpen(false)} 
+            >
                     <nav 
                         className='sidenav__menu'
                         id='sidenav-menu'
+                        style={open ? {} : { display: 'none'} }
                         
                     >
                         <p> chat 1</p>
                         <p>chat 2</p>
                         <p>chat 3</p>
                         <p>chaasdddddddddasdddddddddddddddddddddt 4</p>
+                    </nav>
+
+                    <nav
+                        className='sidenav__menu-preview'   
+                        style={open ? {display: 'none'} : {} }
+                    >
+                        <p>1</p>
+                        <p>2</p>
+                        <p>3</p>
+                        <p>4</p>
                     </nav>
                 
             </div>
