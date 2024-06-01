0afcf533997b31e0e30d1149be24a1b9b0da346a
bukarinevgeni <veliknon@gmail.com>
2024-06-02 01:14:33 +0300

    add modal form

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,13 +1,22 @@
 import type { NextAuthOptions } from "next-auth";
+import { useSession } from "next-auth/react";
+import {redirect} from "next/navigation";
+import { PrismaClient  } from '@prisma/client'
+import bcrypt from "bcryptjs";
+
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+
+const prisma = new PrismaClient();
 
 // Define authentication options using NextAuthOptions interface
 export const authOptions: NextAuthOptions = {
   // Customize authentication pages
-  pages: {
-    signIn: "/login", // Redirect users to "/login" when signing in
-  },
+  // pages: {
+  //   signIn: "/", // Redirect users to "/login" when signing in
+  // },
   // Configure session management
   session: {
     strategy: "jwt", // Use JSON Web Tokens (JWT) for session management
@@ -16,6 +25,40 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXT_PUBLIC_SECRET,
   // Configure authentication providers
   providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        email: { 
+          label: "Email", 
+          type: "email",
+          placeholder: "email@email.com"
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if(!credentials || !credentials.email || !credentials.password) return null;
+
+        const dbUser = await prisma.user.findFirst({
+          where:{
+            email: credentials.email
+          }
+        });
+
+        if(!dbUser) return null;
+
+        const result =  await bcrypt.compare(credentials.password, dbUser?.password as string);
+
+        if(result){
+          return {
+            id: dbUser.id.toString(),
+            email: dbUser.email.toString(),
+            name: dbUser.name.toString(),
+          }
+        }
+        return null;
+
+      }
+    }), 
     GoogleProvider({
       // Configure Google authentication provider with environment variables
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -30,3 +73,4 @@ export const authOptions: NextAuthOptions = {
     // CredentialsProvider({}), // Include a Credentials provider (username/password)
   ],
 };
+
--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -2,33 +2,62 @@
 
 import '@styles/login-form.scss';
 import { signIn } from "next-auth/react";
-// import { useSearchParams, useRouter } from "next/navigation";
-import { oAuth } from '@/lib/actions';
+import { useState } from 'react';
+import {  useRouter } from 'next/navigation';
 import Button from './button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 
 export default function LoginForm() {
+    const[error, setError] = useState<null|string>(null);
     const callbackUrl = "/chat";
+    const router = useRouter();
+    const handleSubmit = async(e) => {
+
+        e.preventDefault();
+        const signInResponse = await signIn("credentials", {
+            email: e.target.email.value,
+            password: e.target.password.value,
+            redirect: false,
+        });
+
+        if( signInResponse && !signInResponse.error){
+            router.push(callbackUrl);
+        }
+        else{
+            setError('Wrong credentials');
+        }
+    }
+
 
     return (
         <section className='login-form block'>
-            <form>
-                <label htmlFor='username'>Username</label>
-                <input type='text' id='username' name='username' />
+            <form
+                onSubmit={handleSubmit}
+            >
+
+                <label htmlFor='email'>Email</label>
+                <input type='text' id='email' name='email' />
                 <label htmlFor='password'>Password</label>
                 <input type='password' id='password' name='password' />
+                {
+                    error && 
+                    <p className='login-form__error'>
+                        {error}
+                    </p>
+                }
                 <Button className='login-form__submit' type='submit'>Login</Button>
-
                 <a
-                    className="login-form__google"
+                    className="login-form__oauth"
                     style={{ backgroundColor: "#ffffff", color: "gray" }}
                     onClick={() => signIn("google", { callbackUrl })}
                     role="button"
                 >
-                    Continue with Google
+                   <FontAwesomeIcon icon={faGoogle} className='login-form__oauth__icon' /> 
+                   Continue with Google
                 </a>
             </form>
-            
         </section>
     );
 }
