f1494016c6cd84c3a32ee0679fb729b0703eda39
bukarinevgeni <veliknon@gmail.com>
2024-06-16 02:54:12 +0300

    Update pubnub.js

--- pubnub.js
+++ pubnub.js
@@ -3,7 +3,7 @@ import PubNub from 'pubnub';
 const pubnub = new PubNub({
   publishKey: process.env.NEXT_PUBLIC_PUBNUB_PUBLISH_KEY,
   subscribeKey: process.env.NEXT_PUBLIC_PUBNUB_SUBSCRIBE_KEY,
-  userId: process.env.NEXT_PUBLIC_PUBNUB_UUID,
+  userId: process.env.NEXT_PUBLIC_PUBNUB_USERID,
 });
 
 export default pubnub;
