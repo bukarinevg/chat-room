89fa7f0add07c3fa0503428e69ee52b7c05813ba
bukarinevgeni <veliknon@gmail.com>
2024-06-16 02:48:07 +0300

    change for userId

--- pubnub.js
+++ pubnub.js
@@ -3,7 +3,7 @@ import PubNub from 'pubnub';
 const pubnub = new PubNub({
   publishKey: process.env.NEXT_PUBLIC_PUBNUB_PUBLISH_KEY,
   subscribeKey: process.env.NEXT_PUBLIC_PUBNUB_SUBSCRIBE_KEY,
-  uuid: process.env.NEXT_PUBLIC_PUBNUB_UUID // Unique identifier for each user
+  userId: process.env.NEXT_PUBLIC_PUBNUB_UUID,
 });
 
 export default pubnub;
