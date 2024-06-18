55f62b516d215325c0753fa99e3a98c5f67bc73c
bukarinevgeni <veliknon@gmail.com>
2024-06-18 04:15:42 +0300

    add sound message sending

--- src/app/components/chat-messages.tsx
+++ src/app/components/chat-messages.tsx
@@ -5,9 +5,9 @@ import ChatMessage from '@components/chat-message';
 import Spinner from '@components/spinner';
 import { Message } from '@/lib/types';
 
-
 import { useRef, useEffect, useState, useContext } from 'react';
 import pubnub from '@root/pubnub';
+import { useSession } from 'next-auth/react';
 
 
 export default function ChatMessages(
@@ -16,7 +16,9 @@ export default function ChatMessages(
 
     const [loading, setLoadingMessages] = useState(true);
     const messagesEndRef = useRef<HTMLDivElement>(null);
-    const audioRef = useRef(new Audio('/notification.mp3'));
+    const audioReceiveRef = useRef<HTMLAudioElement|null>(null);
+    const audioSendRef = useRef<HTMLAudioElement|null>(null);
+    const user = useSession()?.data?.user;
 
 
     const scrollToBottom = () => {
@@ -35,7 +37,16 @@ export default function ChatMessages(
             setMessages(
                 (currentMessages) => {
                 if (event.message.id !== currentMessages[currentMessages.length - 1]?.id) {
-                  audioRef.current.play().catch(error => console.error("Audio play failed", error));
+                  if(audioReceiveRef && audioReceiveRef?.current && audioSendRef && audioSendRef.current){
+                    alert(`user?.id event.message.user.id ${user?.id} ${event.message.user.id}`);
+
+                    if(user?.id !== event.message.user.id){
+                      audioReceiveRef.current.play();
+                    }
+                    else{
+                      audioSendRef.current.play();
+                    }
+                  }
                   return [...currentMessages, event.message];
                 }
                 return currentMessages;
@@ -51,7 +62,9 @@ export default function ChatMessages(
 
     return (
         <div className='chat-messages'>
-          <audio style={{display:'none'}} ref={audioRef} src='/notification.mp3' />
+          <audio style={{display:'none'}} ref={audioReceiveRef} src='/notification.mp3' />
+          <audio style={{display:'none'}} ref={audioSendRef} src='/send.mp3' />
+
             {    
            
                 !loading && chatMessages &&
